bc23e81b1473def3264cec6e9c157f4aea4ecc38
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 21:14:29 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -2,17 +2,22 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://www.sammies.biz/lindenhurst-menu/", {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "en-US,en;q=0.9",
-        "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Microsoft Edge\";v=\"144\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "Sec-Fetch-Site": "none",
+        "Cache-control": "max-age=0",
+        "Connection": "keep-alive",
+        "Host": "www.sammies.biz",        
+        "Sec-Ch-Ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Microsoft Edge\";v=\"144\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
         "Sec-Fetch-Mode": "navigate",
         "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
         "Upgrade-Insecure-Requests": "1",
-        "Referer": "https://www.walmart.com/"
+        "Referer": "https://www.bing.com/" 
       }
     });
 
