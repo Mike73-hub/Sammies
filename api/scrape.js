e067c97b8905b73f4f4909bd1bc36986be820b08
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 21:20:14 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
         "Accept":
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-encoding": "gzip, deflate, br, zstd",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "en-US,en;q=0.9",
         "Cache-control": "max-age=0",
         "Connection": "keep-alive",
