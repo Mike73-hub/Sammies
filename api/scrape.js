e07d5413c35310f2738546841cb49372f8bbb68c
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 21:54:02 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://www.sammies.biz/lindenhurst-menu/", {
+    const response = await fetch("https://www.luciastaqueria.com/", {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
