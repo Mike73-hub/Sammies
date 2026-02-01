e314bc031c588bd51c9ae264c2977f3d871fc8e6
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 22:04:56 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -4,28 +4,10 @@ export default async function handler(req, res) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0",
-        "Accept":
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Cache-control": "max-age=0",
-        "Connection": "keep-alive",
-        "Host": "www.sammies.biz",
-        "Sec-Ch-Ua":
-          "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Microsoft Edge\";v=\"144\"",
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": "\"Windows\"",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Site": "cross-site",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "Referer": "https://www.bing.com/"
       }
     });
 
     const html = await response.text();
-
     return res.status(200).send(html);
 
   } catch (err) {
